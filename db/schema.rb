0057f425d4cfb4c88a96831951c0aa967c700af9
Silvia Di Gianfrancesco <silvia.digianfrancesco@gmail.com>
2015-01-04 11:34:01 +0100

    Add Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102184120) do
+ActiveRecord::Schema.define(version: 20150104102307) do
 
   create_table "cosmetics", force: :cascade do |t|
     t.string   "Product"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150102184120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
